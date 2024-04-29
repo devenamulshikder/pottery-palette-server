4d990ffd529431ac07cb483af8dad6722c32f5bf
devenamulshikder <devenamulshikder@gmail.com>
2024-04-29 08:47:09 +0600

    update methods added

--- index.js
+++ index.js
@@ -29,12 +29,6 @@ async function run() {
 
     const potteryCollectin = client.db("potteryDB").collection("pottery");
 
-    app.post("/pottery", async (req, res) => {
-      const newPottery = req.body;
-      const result = await potteryCollectin.insertOne(newPottery);
-      res.send(result);
-    });
-
     app.get("/pottery", async (req, res) => {
       const cursor = potteryCollectin.find();
       const result = await cursor.toArray();
@@ -48,10 +42,47 @@ async function run() {
       res.send(result);
     });
 
+    // app.get("/pottery/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) };
+    //   const result = await potteryCollectin.findOne(query);
+    //   res.send(result);
+    // });
+
     app.get("/myartcraftlist/:email", async (req, res) => {
       const email = req.params.email;
       const query = potteryCollectin.find({ userEmail: email });
       const result = await query.toArray();
+      res.send(result);
+    });
+
+    app.post("/pottery", async (req, res) => {
+      const newPottery = req.body;
+      const result = await potteryCollectin.insertOne(newPottery);
+      res.send(result);
+    });
+
+    // updated
+    app.put("/pottery/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedCraft = req.body;
+      const craft = {
+        $set: {
+          item_name: updatedCraft.item_name,
+          subcategory_name: updatedCraft.subcategory_name,
+          price: updatedCraft.price,
+          rating: updatedCraft.rating,
+          customizable: updatedCraft.customizable,
+          image: updatedCraft.image,
+          stock: updatedCraft.stock,
+          processing_time: updatedCraft.processing_time,
+          description: updatedCraft.description,
+        },
+      };
+
+      const result = await potteryCollectin.updateOne(filter, craft, options);
       res.send(result);
     });
 
@@ -61,8 +92,6 @@ async function run() {
       const result = await potteryCollectin.deleteOne(query);
       res.send(result);
     });
-
-
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
